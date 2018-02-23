5c52d3e721ee19f944925d37c95ce9332bf14e0c
Junyang Qian <jyqian@DN0a1e8866.SUNet>
2018-02-22 21:42:06 -0800

    Add dynamic pointing decoder

--- code/modules.py
+++ code/modules.py
@@ -78,7 +78,79 @@ class RNNEncoder(object):
 
 
 
+class DPDecoder(object):
+    """
+    TODO: MODIFY THE COMMENT!
 
+    General-purpose module to encode a sequence using a RNN.
+    It feeds the input through a RNN and returns all the hidden states.
+
+    Note: In lecture 8, we talked about how you might use a RNN as an "encoder"
+    to get a single, fixed size vector representation of a sequence
+    (e.g. by taking element-wise max of hidden states).
+    Here, we're using the RNN as an "encoder" but we're not taking max;
+    we're just returning all the hidden states. The terminology "encoder"
+    still applies because we're getting a different "encoding" of each
+    position in the sequence, and we'll use the encodings downstream in the model.
+
+    This code uses a bidirectional GRU, but you could experiment with other types of RNN.
+    """
+
+    def __init__(self, hidden_size, keep_prob, num_iterations):
+        """
+        Inputs:
+          hidden_size: int. Hidden size of the RNN
+          keep_prob: Tensor containing a single scalar that is the keep probability (for dropout)
+        """
+        self.hidden_size = hidden_size
+        self.keep_prob = keep_prob
+        self.num_iterations = num_iterations
+        self.LSTM_dec = tf.nn.rnn_cell.LSTMCell(hidden_size)
+        # self.rnn_cell_fw = rnn_cell.GRUCell(self.hidden_size)
+        # self.rnn_cell_fw = DropoutWrapper(self.rnn_cell_fw, input_keep_prob=self.keep_prob)
+        # self.rnn_cell_bw = rnn_cell.GRUCell(self.hidden_size)
+        # self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
+
+    def build_graph(self, U):
+        """
+        Inputs:
+          inputs: Tensor shape (batch_size, seq_len, input_size)
+          masks: Tensor shape (batch_size, seq_len).
+            Has 1s where there is real input, 0s where there's padding.
+            This is used to make sure tf.nn.bidirectional_dynamic_rnn doesn't iterate through masked steps.
+
+        Returns:
+          out: Tensor shape (batch_size, seq_len, hidden_size*2).
+            This is all hidden states (fw and bw hidden states are concatenated).
+        """
+        with vs.variable_scope("DPDecoder"):
+
+            # input_lens = tf.reduce_sum(masks, reduction_indices=1) # shape (batch_size)
+
+            # Note: fw_out and bw_out are the hidden states for every timestep.
+            # Each is shape (batch_size, seq_len, hidden_size).
+
+            initial_state = tf.get_variable("initial_state", shape=(hidden_size), dtype=tf.float32)
+
+            hidden = tf.reshape(tf.tile(initial_state, multiples=tf.shape(U)[0]), [-1, hidden_size])
+            s = start_pos
+            e = end_pos
+            for _ in range(self.num_iterations):
+                hidden = self.LSTM_dec(tf.concat([U[:,s], U[:,e]], 1), hidden)
+                alpha = self.HMN_start(U, hidden, s, e)
+                beta = self.HMN_end(U, hidden, s, e)
+                s = tf.argmax(alpha, axis=1)
+                e = tf.argmax(beta, axis=1)
+
+            # (fw_out, bw_out), _ = tf.nn.dynamic_rnn(self.lstm_cell, inputs, input_lens, dtype=tf.float32)
+
+            # # Concatenate the forward and backward hidden states
+            # out = tf.concat([fw_out, bw_out], 2)
+
+            # # Apply dropout
+            # out = tf.nn.dropout(out, self.keep_prob)
+
+            return s, e
 
 
 class SimpleSoftmaxLayer(object):
