dd71471d5f5ba83d81a14856eae8648401cad8de
Junyang Qian <jyqian@DN0a1ea217.SUNet>
2018-03-13 17:24:27 -0700

    840 glove vec allowed.

--- code/vocab.py
+++ code/vocab.py
@@ -45,7 +45,10 @@ def get_glove(glove_path, glove_dim):
     """
 
     print "Loading GLoVE vectors from file: %s" % glove_path
-    vocab_size = int(4e5) # this is the vocab size of the corpus we've downloaded
+    if os.path.basename(your_path) == "glove.840B.300d.txt":
+        vocab_size = int(2.2e6)
+    else:
+        vocab_size = int(4e5) # this is the vocab size of the corpus we've downloaded
 
     emb_matrix = np.zeros((vocab_size + len(_START_VOCAB), glove_dim))
     word2id = {}
